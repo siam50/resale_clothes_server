8a6726dbf5f6956009a4df86182e19b1fbcdfdd7
siam50 <siamurrahman50@gmail.com>
2022-11-24 20:58:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1obyivv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('resaleClothes').collection('categories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+app.get('/', async (req, res) => {
+    res.send('resale clothes server is running');
+})
+
+app.listen(port, () => console.log(`resale clothes running on ${port}`))
