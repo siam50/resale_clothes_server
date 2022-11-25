fd7e6d0e5abac0b2e3e371d62c8115cc50a2684f
siam50 <siamurrahman50@gmail.com>
2022-11-25 23:18:39 +0600

    all buyers data loaded

--- index.js
+++ index.js
@@ -51,9 +51,15 @@ async function run() {
 
         // Users API
         app.get('/users', async (req, res) => {
-            const query = req.body;
+            const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users)
+        });
+
+        app.get('/users/buyers', async (req, res) => {
+            const query = { role: "buyer" };
+            const buyers = await usersCollection.find(query).toArray();
+            res.send(buyers)
         });
 
         app.post('/users', async (req, res) => {
@@ -67,7 +73,14 @@ async function run() {
             const query = { email }
             const user = await usersCollection.findOne(query);
             res.send({ isSeller: user?.role === 'seller' });
-        })
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin' });
+        });
     }
     finally {
 
